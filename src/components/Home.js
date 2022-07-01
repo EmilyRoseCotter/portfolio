883ca40fa8a03a477e953d24ab6418ff36f465b6
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-07-01 10:53:18 +0100

    Added an image and some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/Home.css";
+import Emily from "../Assets/Emily.png";
 
 const Home = () => {
   return (
     <div className="Home">
       <div className="Hero">
         <h1>Hello, I&apos;m Emily.</h1>
+        <img className="Avatar" src={Emily} alt="Emily" />
       </div>
     </div>
   );
