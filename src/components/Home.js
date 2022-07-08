47c0771e632ac9624d15d790c5865129338bb2d0
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-07-08 14:05:37 +0100

    changed hero layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,15 @@ const Home = () => {
   return (
     <div className="Home">
       <div className="Hero">
-        <h1>Hello, I&apos;m Emily.</h1>
-        <img className="Avatar" src={Emily} alt="Emily" />
-        <p className="subTitle">An aspiring Software Engineer</p>
+        <div className="heroHeadings">
+          <h1>Hello, I&apos;m Emily.</h1>
+          <h2 className="subTitle">
+            An aspiring Software Engineer <br /> from the North-West of England.
+          </h2>
+        </div>
+        <div className="heroImage">
+          <img className="Avatar" src={Emily} alt="Emily" />
+        </div>
       </div>
     </div>
   );
